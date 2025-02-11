0250be49ff04f3724d64e984120bfbb5f300af62
yxd <yxdaysthj@outlook.com>
2025-02-11 16:14:29 +0800

    update

--- QuaternionRotation/Main.cpp
+++ QuaternionRotation/Main.cpp
@@ -18,14 +18,14 @@ int main(int argc, char* argv[])
 
 	double num = 123.456789;
 
-	// 使用 fixed 和 setprecision 控制输出
-	std::cout << std::fixed << std::setprecision(2) << num << std::endl;  // 输出: 123.46
+	//// 使用 fixed 和 setprecision 控制输出
+	//std::cout << std::fixed << std::setprecision(2) << num << std::endl;  // 输出: 123.46
 
-	// 重置格式化标志
-	std::cout << std::resetiosflags(std::ios_base::fixed | std::ios_base::floatfield);
+	//// 重置格式化标志
+	//std::cout << std::resetiosflags(std::ios_base::fixed | std::ios_base::floatfield);
 
-	// 恢复默认格式
-	std::cout << num << std::endl;  // 输出: 123.457（默认格式）
+	//// 恢复默认格式
+	//std::cout << num << std::endl;  // 输出: 123.457（默认格式）
 
 
 	return 0;
